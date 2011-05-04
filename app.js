b256c4b98f68e2fc52fc6118df9998669b64b6ca
Darren Shafae <dshafae@papercheck.com>
2011-05-03 22:15:16 -0700

    ExpressJS update.

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-//var express = require('express');
-var app = require('express').createServer();
+var express = require('express');
+var app = express.createServer();
 
-app.get('/', function(req, res){
-  res.send('hello world');
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
+});
+
+app.configure('development', function(){
+    app.use(express.static(__dirname + '/www'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/www', { maxAge: oneYear }));
+  app.use(express.errorHandler());
 });
 
 app.listen(3000);
